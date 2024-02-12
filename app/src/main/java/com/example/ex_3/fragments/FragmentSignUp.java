94064b82e9d1dd6408285146fe9e2982f9d988ab
Orel-Natan <O.T.Natan@gmail.com>
2024-02-12 17:30:17 +0200

    fix the navigation from button Go to the Fragmentrecycleview

--- app/src/main/java/com/example/ex_3/fragments/FragmentSignUp.java
+++ app/src/main/java/com/example/ex_3/fragments/FragmentSignUp.java
@@ -3,10 +3,12 @@ package com.example.ex_3.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.ex_3.R;
 
@@ -62,6 +64,14 @@ public class FragmentSignUp extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        Button button = view.findViewById(R.id.buttonGo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_fragmentSignUp_to_fragmentRecycleView);
+
+            }
+        });
 
 
         return view;
