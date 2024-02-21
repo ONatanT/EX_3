6fb6884304a055237cf04b16b60a45f3a8cf4e7b
Orel-Natan <O.T.Natan@gmail.com>
2024-02-21 20:17:40 +0200

    connect the firebase into login fragment and signup fragment

--- app/src/main/java/com/example/ex_3/fragments/FragmentLogIn.java
+++ app/src/main/java/com/example/ex_3/fragments/FragmentLogIn.java
@@ -3,6 +3,7 @@ package com.example.ex_3.fragments;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -11,10 +12,16 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
 import android.widget.EditText;
 
 import com.example.ex_3.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -54,6 +61,7 @@ public class FragmentLogIn extends Fragment {
         return fragment;
     }
 
+    private FirebaseAuth mAuth;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,6 +69,7 @@ public class FragmentLogIn extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -107,4 +116,22 @@ public class FragmentLogIn extends Fragment {
 
         return view;
     }
+
+    public void loginFunc(View view) {
+        String email = ((EditText) view.findViewById(R.id.editTextUsername) ).getText().toString().trim();
+        String password = ((EditText) view.findViewById(R.id.editTextPassword) ).getText().toString().trim();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                        }
+                    }
+                });
+    }
+
 }
